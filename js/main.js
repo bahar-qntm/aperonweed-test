30eae9028ccb0d0b6b723e1e53a8a5eef7141a12
bahar-qntm <116527214+bahar-qntm@users.noreply.github.com>
2023-01-24 17:44:34 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -89,10 +89,12 @@ snapshotButton.onclick = function() {
 
   //var url = "https://aperonncus-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/1fc5c98b-7c0d-4fd3-b41f-4356f78310c9/classify/iterations/Iteration1/image"
   var url = "https://qntm-apim.azure-api.net/KnowYourWeed/classify"
-
+  //var url = "https://qntm-apim.azure-api.net/KnowYourWeed-Backup/classify"
   var xhr = new XMLHttpRequest();
   xhr.open("POST", url);
 
+  //xhr.setRequestHeader("Prediction-Key", "8b70c86275ae4765902cdc5a25e84435");
+  //xhr.setRequestHeader("Content-Type", "application/octet-stream");
   xhr.setRequestHeader("Ocp-Apim-Subscription-Key", "39ee06e8c47940f78abb8fee0036796a");
   xhr.setRequestHeader("Content-Type", "multipart/form-data");
   //xhr.setRequestHeader("Content-Length", "0");
